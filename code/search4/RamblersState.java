6b01e908daab3c3a0ecb544e8011f057c703f87f
Antrea Koulouteri <antreakoulouteri@gmail.com>
2021-05-18 00:55:14 +0300

    fixed some errors

--- code/search4/RamblersState.java
+++ code/search4/RamblersState.java
@@ -19,7 +19,7 @@ public class RamblersState extends SearchState {
   // goalPredicate
   public boolean goalPredicate(Search searcher) {
     RamblersSearch rsearcher = (RamblersSearch) searcher;
-    String goal = rsearcher.getGoal(); 
+    Coords goal = rsearcher.getGoal(); 
     return (coordinate == goal);
   }
 
@@ -30,17 +30,17 @@ public class RamblersState extends SearchState {
     ArrayList<SearchState> succs = new ArrayList<SearchState>();
  
 
-    for (int i=0; i<map.length; i++) {
-      Coords currX = coordinate.getx();
-      Coords currY = coordinate.gety();
+    for (int i=0; i<map.getHeight(); i++) {
+      int currX = coordinate.getx();
+      int currY = coordinate.gety();
       int lc = 0;
       if (currX < 15 && currY < 15) {
-        if ( (currY.getHeight() && currX.getHeight()) <= ( map.gety().getHeight() && map.getx().getHeight()) ) {
+        if ( (currY.getHeight() && currX.getHeight()) <= ( coordinate.gety().getHeight() && coordinate.getx().getHeight()) ) {
               lc = lc +1;
             }
 
         else {
-        lc = lc +1 + ( (currY.getHeight() && currX.getHeight()) - (map.gety().getHeight() && map.getx().getHeight()) );
+        lc = lc +1 + ( (currY.getHeight() && currX.getHeight()) - (coordinate.gety().getHeight() && coordinate.getx().getHeight()) );
             };
       
       succs.add((SearchState) new RamblersState(coordinate, lc));
@@ -52,7 +52,7 @@ public class RamblersState extends SearchState {
   // sameState
   public boolean sameState(SearchState s2) {
     RamblersState rs2 = (RamblersState) s2;
-    return (coordinate.compareTo(rs2.getCoords()) == 0);
+    return (coordinate == rs2.getCoords());
   }
 
   // toString
